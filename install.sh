6428495a9ac5c59182e2fe4e160c5d8eb3167163
thoverik <tho.pham@veriksystems.com>
2021-12-31 12:49:59 +0700

    update install script

--- install.sh
+++ install.sh
@@ -11,19 +11,7 @@ kubectl version --client
 
 # install Helm
 # https://helm.sh/docs/intro/install/#from-script
-curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | sh
-
-# install S3 plugin for Helm
-helm plugin install https://github.com/hypnoglow/helm-s3.git --version $HELM_S3_PLUGIN_VERSION
-
-# aws-iam-authenticator
-# https://docs.aws.amazon.com/eks/latest/userguide/install-aws-iam-authenticator.html
-curl -o aws-iam-authenticator https://amazon-eks.s3.us-west-2.amazonaws.com/1.21.2/2021-07-05/bin/linux/amd64/aws-iam-authenticator
-curl -o aws-iam-authenticator.sha256 https://amazon-eks.s3.us-west-2.amazonaws.com/1.21.2/2021-07-05/bin/linux/amd64/aws-iam-authenticator.sha256
-openssl sha1 -sha256 aws-iam-authenticator
-chmod +x ./aws-iam-authenticator
-mv aws-iam-authenticator /usr/local/bin/aws-iam-authenticator
-aws-iam-authenticator help
+curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | sh
 
 # install eksctl 
 curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
